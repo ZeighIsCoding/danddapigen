d8ed8ec832734c639d20a10a8db1fa6142a97c33
Zain Sheikh <zainsheikh61506@gmail.com>
2024-04-26 12:10:38 -0400

    saviour commit

--- src/about.js
+++ src/about.js
@@ -9,24 +9,28 @@ class About extends React.Component {
         super(props);
     
         this.state = {
-          items: [],
+            className: "",
           DataisLoaded: false,
         };
       }
     
         componentDidMount() {
-          fetch("https://www.dnd5eapi.co")
+          fetch("https://www.dnd5eapi.co/api/classes")
             .then((res) => res.json())
             .then((json) => {
+                console.log(json);
+                
+                console.log("dtdtd ", json.results[0].index)
+
               this.setState({
-                items: json,
+                className: json.results[0].index,
                 DataisLoaded: true,
             });
           });
         }
 
         render() {
-            const { DataisLoaded, items } = this.state;
+            const { DataisLoaded, className } = this.state;
             if (!DataisLoaded)
                 return (
                     <div>
@@ -35,8 +39,11 @@ class About extends React.Component {
                   );
 
     return (
+        <div>
         <h1>This is a test to see if you still got ya apis holmes</h1>
-        
+        <h3>Class Name: {className}</h3>
+        </div>
+
     );
 }
 }
